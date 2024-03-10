4ea2a373572b62f2bc37f94ae3ea0ebc88b31f19
Oscar Blessed <oscarblessed04@gmail.com>
2024-03-10 08:16:46 +0100

    user admin setup"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,31 @@
 from django.contrib import admin
 from accounts.models import User
+from django.contrib.auth.hashers import make_password
 
-admin.site.register(User)
+class UserResourceAdmin(admin.ModelAdmin):
+    search_fields = [
+        "email",
+        "full_name",
+        "is_active"
+    ]
+    list_per_page = 50
+    date_hierarchy = "date_joined"
+
+    def get_list_display(self, request):
+        to_be_displayed = [field.name for field in self.model._meta.concrete_fields]
+        to_be_displayed.remove("password")
+
+        return to_be_displayed
+    
+    def save_model(self, request, obj, form, change):
+        
+        # Hash the password before saving
+        if "password" in form.changed_data:
+            obj.password = make_password(form["password"].value())
+
+        super().save_model(request, obj, form, change)
+
+
+
+admin.site.register(User, UserResourceAdmin)
+    
